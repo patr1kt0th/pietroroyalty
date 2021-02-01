7516f1b99ffb493d9823faf07aae03e2e3b46798
ptoth <patrik.toth@sk.ibm.com>
2021-02-01 21:30:13 +0100

    Web version

--- src/app/models/tag.model.ts
+++
@@ -1,9 +0,0 @@
-export class Tag {
-  selected = false;
-
-  constructor(public name: string) {}
-
-  get label() {
-    return 'tag.' + this.name;
-  }
-}
--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  base: '/',
-  // base: '/pietroroyalty/',
-  apiUrl: 'http://localhost:8080'
-};
